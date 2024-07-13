d98e733ded62dff8116688b8b23eec7f31efb6af
rostamrasuli <rostam.homayoon@gmail.com>
2024-07-12 22:58:04 -0400

    fb.com

---
+++ src/test/java/tek/intro/handlechrome.java
@@ -0,0 +1,15 @@
+package tek.intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class handlechrome {
+    public static void main(String[] args) {
+
+       ChromeDriver ChromeDriver = new ChromeDriver();
+       ChromeDriver.get("https:facebook.com");
+
+
+
+
+    }
+}
